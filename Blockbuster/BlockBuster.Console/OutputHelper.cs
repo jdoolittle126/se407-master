7e7e642ce6e198c0903e1a1194ca186d67940e89
Jonathan <jdoolittle126@gmail.com>
2022-02-01 20:35:21 -0500

    Week 4

---
+++ Blockbuster/BlockBuster.Console/OutputHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlockBusterLibrary.Models;
+using CsvHelper;
+
+namespace BlockBuster.Console
+{
+    class OutputHelper
+    {
+
+        public void WriteToConsole(List<Movie> movies)
+        {
+            System.Console.WriteLine("-- LIST OF MOVIES --");
+            movies.ForEach(movie =>
+            {
+                System.Console.WriteLine(FormatMovieString(movie));
+            });
+        }
+
+        public void WriteToCsv(List<Movie> movies)
+        {
+            using var writer = new StreamWriter("");
+            using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            csv.WriteRecords(movies);
+        }
+
+        private static string FormatMovieString(Movie movie)
+        {
+            return $"ID: {movie.MovieId,-5} TITLE: {movie.Title,-25} RELEASED: {movie.ReleaseYear,4}";
+        }
+
+    }
+}
---
+++ Blockbuster/BlockBuster.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BlockBuster.Console
+{
+    internal class Program
+    {
+        private enum OutputTypes {
+            Console,
+            Csv
+        }
+
+        private static void Main(string[] args)
+        {
+            OutputHelper o = new OutputHelper();
+            o.WriteToConsole(BlockBusterLibrary.BlockBusterBasicFunctions.GetAllMovies());
+        }
+    }
+}
